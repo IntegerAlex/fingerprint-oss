b4d2aee7b6da391161014114460a534173c23fe0
akshat k <inquiry.akshatkotpalliwar@gmail.com>
2025-03-16 10:50:48 +0530

    Init Hash

--- src/hash.ts
+++ src/hash.ts
@@ -1,9 +1,7 @@
 /**
- * Create a unique hash of the system information.
- * @param {info object} info
-*  @returns {string} hash
-*  @example
-* 	const hash = createHash(info);
-*  	console.log(hash);
-*  	=> 'a1b2c3d4e5f6g7h8i9j0'
-*/
+ * Generate a Hash from the system info to identify the user
+ * @param systemInfo 
+ * @returns hash string
+ */
+
+
