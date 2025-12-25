19d277f54547b059196c808471120ecb88469a43
IntegerAlex <inquiry.akshatkotpalliwar@gmail.com>
2025-12-25 05:21:04 +0000

    demo: update demo app for v0.9.2 browser field

--- fingerprint-oss-demo/app/page.tsx
+++ fingerprint-oss-demo/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useCallback, useMemo } from "react";
-import userInfo from "../../dist/index.esm.js";
+import userInfo from "fingerprint-oss";
 import { Loader2 } from "lucide-react";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
